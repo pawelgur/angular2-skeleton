ccadb1951e68667acaf711c21435690424e1dd4d
pawelgur <pawelgur@gmail.com>
2016-10-29 13:32:38 +0200

    switched target to es5, added investigation related comments

--- webpack.aot.config.js
+++ webpack.aot.config.js
@@ -3,6 +3,9 @@ let path = require('path');
 let webpack = require('webpack');
 let ngtools = require('@ngtools/webpack');
 
+// TIP: if compilation fails try to run ngc only, then tsc only (in that order) - it's possible that the problem
+//      is in ngc or tsc. Webpack's autoPlugin doesn't show real errors (fixed ts libs and it worked)
+
 module.exports = {
     entry: {
         'main.aot': './src/main.aot.ts',
@@ -30,12 +33,14 @@ module.exports = {
     },
 
     plugins: [
+        // NOTE: possibly this should replace ngc (use it internally?) - build works without it
+        //       but there's cannot find ngfactories errors when using this without ngc (soemthing wrong with paths possibly)
         new ngtools.AotPlugin({
             tsConfigPath: './tsconfig.aot.json',
-            baseDir: path.resolve(__dirname, ''),
+            baseDir: path.resolve(__dirname, 'src'),
             entryModule: path.join(__dirname, 'src', 'app', 'app.module') + '#AppModule'
         }),
-        // new webpack.ProgressPlugin(),
+        new webpack.ProgressPlugin(),
         new webpack.ContextReplacementPlugin(
             // The (\\|\/) piece accounts for path separators in *nix and Windows
             /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
